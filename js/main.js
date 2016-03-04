d93840d57bd5c415796632e7b588f1c912286a86
Justin Tanacredi <jtanacredi@gmail.com>
2016-03-04 00:51:44 -0500

    Modal text color fix.

--- js/main.js
+++ js/main.js
@@ -1,27 +1,23 @@
 var newString, newColor, modalColors;
+modalColors = document.getElementsByClassName("modal-colors");
+borderColors = document.getElementsByClassName("border-colors");
 
 function waitPivot() {
-  newString = Randomize.run(words);
-  newColor = Randomize.randomColor(colors);
-
   function mapper(elementArray,applyStyle) {
     for (var i = 0; i < elementArray.length; i++) {
-      elementArray[i].style.applyStyle = newColor;
+      elementArray[i].style[applyStyle] = newColor;
     }
   }
-
+  newString = Randomize.run(words);
+  newColor = Randomize.randomColor(colors);
   document.getElementById('replace-this-text').innerHTML = newString;
-  // document.getElementById('divider').style.borderColor = newColor;
   document.body.style.backgroundColor = newColor;
-  modalColors = document.getElementsByClassName("modal-colors");
-  borderColors = document.getElementsByClassName("border-colors");
-  mapper(borderColors,"borderColor");
+  mapper(borderColors,"border-color");
   mapper(modalColors,"color");
-  // for (var i = 0; i < modalColors.length; i++) {
-  //   modalColors[i].style.color = newColor;
-  // }
 }
+
 waitPivot();
+
 
 document.getElementById('pivot').onclick = function() {
   waitPivot();
